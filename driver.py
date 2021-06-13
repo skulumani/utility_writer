7ae2693b6508d34b085d0a3234a57c8a68755bb0
Shankar Kulumani <shanks.k@gmail.com>
2021-06-13 18:10:13 -0400

    Update driver

--- driver.py
+++ driver.py
@@ -12,3 +12,9 @@ eclient.login(username, password)
 # check on timezone
 date = datetime(2021, 01, 01, 0, 0, 0)
 times, powers = eclient.system_data(date)
+
+
+# get panel level power data for a date
+# get total system energy for a day
+# write to influxdb
+# loop to next day
