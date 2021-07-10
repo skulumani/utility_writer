768caf55a3e17f1bb706ce88569b78ece990d37c
Shankar Kulumani <shanks.k@gmail.com>
2021-07-10 14:04:47 -0400

    Add config example

---
+++ config_example.py
@@ -0,0 +1,9 @@
+'''Secrets file'''
+
+# Enphase enlighten contractor credentials
+username = "<contractor email account at enlighten"
+password = "<password>"
+
+# InfluxDB credentials
+url = "<ip of influxdb>"
+token = "<write token for bucket>"
